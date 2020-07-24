f97545512e53461b7e98238373c973b22eb1282e
Alexotieno1717 <alexotieno900@gmail.com>
2020-07-24 09:55:27 +0300

    Added thursday kata

--- js/script.js
+++ js/script.js
@@ -7,12 +7,26 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5
  below the number passed in.
 */
 function solution(number){
-    let total = 0;
-    for (i=0; i < number; i++){
-      if(i % 3 === 0 || i % 5 === 0){
-        total +=i;
-      }
+  let total = 0;
+  for (i=0; i < number; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      total +=i;
     }
-    return total;
   }
-  solution(23);
+  return total;
+}
+solution(23);
+
+//Thursday Kata
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+*/
+function createPhoneNumber(numbers){
+  let phoneNumber = "(xxx) xxx-xxxx";
+  
+  for(i = 0; i < numbers.length; i++){
+    phoneNumber = phoneNumber.replace('x', numbers[i]);
+  };
+  return phoneNumber
+}
+
